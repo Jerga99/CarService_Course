51c3aaabb27b9f807f0079855939497278a54d8d
Filip Jerga <jerga99@gmail.com>
2020-01-03 12:13:53 +0100

    3 Variables + Debugging

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ let birthday; // Undefined
 
 let phone = null; // Null
 
+let fullName = 'Filip Jerga';
+
+fullName = 'John Green';
+
+console.log(fullName);
+
 console.log('Name: ' + firstName + ' ' + lastName + ' ,age: ' + age);
 
 console.log(birthday);
