71ae1074483b62b85d5ae5ba211c54e27f37ec70
Filip Jerga <jerga99@gmail.com>
2020-01-03 11:43:30 +0100

    2 Variables Introduction

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 
-console.log('Hello World');
+
+// VARIABLES
+
+let isEmployed = true; // Boolean
+let hasApartment = false;
+
+let firstName = 'Filip'; // String
+let lastName = 'Jerga';
+let age = 29; // Number
+
+let birthday; // Undefined
+
+let phone = null; // Null
+
+console.log('Name: ' + firstName + ' ' + lastName + ' ,age: ' + age);
+
+console.log(birthday);
+
+console.log(phone);
