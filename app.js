5da3a779f8c6c017c1be19ee79bc41f7d6fe64d9
Filip Jerga <jerga99@gmail.com>
2020-01-04 15:31:08 +0100

    9 Return from function

--- app.js
+++ app.js
@@ -3,28 +3,37 @@
 function runApp() {
   // object
   const car = {
-      brand: 'mercedes',
-      type: 'benz',
-      produceYear: 1995,
-      displayInfo: function() {
-        console.log(`${this.brand} ${this.type} - ${this.produceYear}`);
-      }
+    brand: 'mercedes',
+    type: 'benz',
+    produceYear: 1995,
+    displayInfo: function() {
+      console.log(`${this.brand} ${this.type} - ${this.produceYear}`);
+    },
+    getThis: function() {
+      return this;
+    }
   }
 
+  console.log(car === car.getThis());
+
   const car2 = {
-      brand: 'mitshubishi',
-      type: 'lancer',
-      produceYear: 2002 
+    brand: 'mitshubishi',
+    type: 'lancer',
+    produceYear: 2002 
   }
 
   // displayCarInfo
   // params: brand, type, year
   function displayCarInfo(car) {
-      console.log(`${car.brand} ${car.type} - ${car.produceYear}`);
+    console.log(`${car.brand} ${car.type} - ${car.produceYear}`);
+    return function() {
+      console.log('I AM FUNCTION')
+    }
   }
 
-  displayCarInfo(car);
-  displayCarInfo(car2);
+  displayCarInfo(car)();
+  const testFunction = displayCarInfo(car2);
+  testFunction();
 }
 
 runApp();
