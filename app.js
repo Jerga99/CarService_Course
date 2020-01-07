1b5bb0386ae52826f16f034438c1ae282537dc14
Filip Jerga <jerga99@gmail.com>
2020-01-07 20:05:11 +0100

    18 forEach, map, filter

--- app.js
+++ app.js
@@ -6,16 +6,39 @@ Object.prototype.displayInfo = function() {
   console.log('DISPLAYING OBJECT INFO!!!!!!');
 }
 
+Array.prototype.forEach2 = function(callback) {
+
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+}
+
 function runApp() {
   const car = new Car({brand: 'mercedes', type: 'benz', year: 1995});
   const car2 = new Car({type: 'lancer', brand: 'mitshubishi', year: 2002});
-
+  const car3 = new Car({type: 'x6', brand: 'bmw', year: 2000});
+  const car4 = new Car({type: 'x4', brand: 'bmw', year: 2005});
   const racingCar = new RacingCar({category: 'k1', brand: 'subaru', type: 'impreza', year: 2010});
+                        
+  const cars = [car, car2, racingCar, car3, car4];
 
-  racingCar.displayInfo();
-  car.displayInfo();
-                          
-  const cars = [car, car2, racingCar];
+  // function displayCarInfo(car) {
+  //   car.displayInfo();
+  // }
+
+  const carBrands = cars.map(function(car) {
+    return car.brand;
+  })
+
+  const filteredCars = cars.filter(function(car) {
+    return car.brand === 'bmw';
+  })
+
+  console.log(filteredCars);
+
+  // cars.forEach2(function(car) {
+  //   car.displayInfo();
+  // })
 }
 
 runApp();
