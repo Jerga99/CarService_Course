0d0e5488f5e9a42c6a69617dc911d13ad987672e
Filip Jerga <jerga99@gmail.com>
2020-01-07 10:56:03 +0100

    13 Arrays

--- app.js
+++ app.js
@@ -2,39 +2,33 @@
 const Car = require('./Car');
 
 function runApp() {
-  const car = new Car('mitshubishi', 'benz', 1995);
+  const car = new Car('mercedes', 'benz', 1995);
   const car2 = new Car('mitshubishi', 'lancer', 2002);
+                          
+  // const carBrands = ['mitshubishi', 'mercedes'];
 
-  // if (car.brand === car2.brand) {
-  //   car.displayInfo();
-  //   car2.displayInfo();
-  // }
+  // console.log(carBrands[0]);
+  // console.log(carBrands[1]);
 
-  const someCondition = false;
+  // carBrands.push('audi');
+  // carBrands.push('skoda');
 
-  if (!someCondition) {
-    console.log('Executing if block')
-  } else {
-    console.log('Executing else block')
+  // console.log(carBrands);
+
+  // carBrands.splice(2, 2);
+
+  // console.log(carBrands);
+
+  const cars = [car, car2];
+
+  cars[0].displayInfo();
+  cars[1].displayInfo();
+
+
+  for(let i = 0; i < 10; i++) {
+    console.log(i);
   }
 
-
-  // == : equal to
-  // === : equal to (strict)
-  // != : not equal to
-  // !== : not equal to(strict)
-  // > : greater than
-  // < : less to
-  // >= : greater than or equal to
-  // >= : less than or equal to
-
-  // if (0 > 10) {
-  //   console.log('Hello World')
-  // } else if(2 > 10) {
-  //   console.log('Executing else if statement')
-  // } else {
-  //   console.log('Executing else statement')
-  // }
 
 }
 
