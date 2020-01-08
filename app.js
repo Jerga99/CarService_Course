c7847880a91e9741f11d7f751b256c2e88e6ed22
Filip Jerga <jerga99@gmail.com>
2020-01-08 12:36:44 +0100

    23 Car Storage

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+const CarStorage = require('./src/car/CarStorage'); 
 const Car = require('./src/car/Car');
 const RacingCar = require('./src/car/RacingCar');
 const readline = require('readline-sync');
@@ -13,14 +14,21 @@ function runApp() {
     console.log('Press "3" to list all of the cars');
     console.log('Press "exit" to quit to program');
   }
-
   const car = new Car({brand: 'mercedes', type: 'benz', year: 1995});
   const car2 = new Car({type: 'lancer', brand: 'mitshubishi', year: 2002});
   const car3 = new Car({type: 'x6', brand: 'bmw', year: 2000});
   const car4 = new Car({type: 'x4', brand: 'bmw', year: 2005});
   const racingCar = new RacingCar({category: 'k1', brand: 'subaru', type: 'impreza', year: 2010});
-                        
+                      
   const cars = [car, car2, racingCar, car3, car4];
+  const carStorage = new CarStorage([...cars]);
+
+  // copying by reference - Objects, Arrays
+  // copying by value - string, boolean, numbers ....
+
+  // cars.forEach(function(car) {
+  //   carStorage.addCar(car);
+  // })
 
   let appIsRunning = true;
   displayOptions();
@@ -36,9 +44,7 @@ function runApp() {
         console.log('removing car car');
         break;
       case '3':
-        cars.forEach(function(car) {
-          car.displayInfo();
-        });
+        carStorage.listCars();
         break;
       case 'exit':
         console.log('Good Bye!');
@@ -47,21 +53,6 @@ function runApp() {
       default:
         console.log('Invalid Option!');
     }
-    
-    // if (userInput === '1') {
-    //   console.log('Creating car');
-    // } else if (userInput === '2') {
-    //   console.log('removing car car');
-    // } else if (userInput === '3') {
-      // cars.forEach(function(car) {
-      //   car.displayInfo();
-      // });
-    // } else if (userInput === 'exit') {
-      // console.log('Good Bye!');
-      // appIsRunning = false;
-    // } else {
-    //   console.log('Invalid Option!');
-    // }
   }
 }
 
